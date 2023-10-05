170c4ec2805c1134a76dd9a592cf5bd8be6088e4
unknown <BT_2N2_19@BKTECH.LOCAL>
2023-10-05 14:05:08 -0400

    Strings!

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class RectangleRunner {
+    public static void main(String[] args) {
+        String blah = "What's for dinner?";
+        String ugh = blah.substring(11,17);
+        System.out.println(ugh);
+    }
+}
